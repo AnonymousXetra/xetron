e2b39e98701f53db4854207f43358311f4c43b90
AnonymousXetra <120418304+AnonymousXetra@users.noreply.github.com>
2022-12-13 06:07:48 +0530

    Add files via upload

---
+++ files/license/hxjhxjv.js
@@ -0,0 +1,5 @@
+function showAlert() { alert('Invalid License Key'); }
+if (window.location.hostname !== 'ddndfndgn.blogspot.com'){
+    showAlert();
+    window.top.location.href = 'https://revamped-lantro-ui.blogspot.com';
+    }
